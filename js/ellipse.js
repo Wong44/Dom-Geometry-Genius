13d7ca2dccb686a731e93a5dfaa749804fa4f661
wong44 <wongmarma234@gmail.com>
2024-09-28 04:39:37 -0400

    first commit

---
+++ js/ellipse.js
@@ -0,0 +1,9 @@
+function calculateEllipseArea(){
+    const a = getInputValue('ellipse-a')
+    const b = getInputValue('ellipse-b');
+
+    const area = 3.14 * a * b;
+
+    setInnerText('ellipse-area', area);
+
+}
