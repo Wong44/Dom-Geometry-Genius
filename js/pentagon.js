334db1a0bbd3de47652826992e08c34321b51740
wong44 <wongmarma234@gmail.com>
2024-09-28 22:14:51 +0600

    first commit

--- js/pentagon.js
+++ js/pentagon.js
@@ -2,7 +2,7 @@ function calculatePentagonArea(){
     const p = getInputValue('pentagon-p')
     const b = getInputValue('pentagon-b');
 
-    const area = 0.5 * p * b
+    const area = 0.5 * p * b;
 
     setInnerText('pentagon-area', area);
 }
